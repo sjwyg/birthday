a0fb7dcb4f8aadaeea59bcc7eaed28320bfedb20
sjwyg <64006570+sjwyg@users.noreply.github.com>
2020-04-20 18:56:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
         "身边依旧还是你",
         "今天要记得吃好的哦",
         "要把我家最好的宝贝喂饱饱",
-        "然后抱走",
+        "然后抱走，然后永远在一起",
         "YAMI~~",
     ],
     /**
@@ -31,12 +31,12 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐乐",
+        play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "收下祝福",
     }
 };
