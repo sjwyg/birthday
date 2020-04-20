5f4d55eae5873a2a73bf2812ca5cf63b324eed25
sjwyg <64006570+sjwyg@users.noreply.github.com>
2020-04-20 18:33:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
+        play: "音乐乐乐乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
